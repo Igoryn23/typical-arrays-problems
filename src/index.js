fbb1918a4c65cc83aad864a032f6db727da4826e
igoryn23 <igoryn9702@gmail.com>
2021-03-15 12:42:06 +0300

    fix: array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array.length == 0 || array == []) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (array.length == 0 || array == []) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
