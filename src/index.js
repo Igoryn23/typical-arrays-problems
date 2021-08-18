ae577f88e809de9ff57c983e68a23f83fc7a6018
Igoryn23 <igoryn9702@gmail.com>
2021-08-18 11:36:37 +0300

    fix: case for empty array

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 exports.min = function min(array) {
-    if (array.length == 0 || array == []) {
+    if (!array || array.length === 0) {
         return 0;
-    } else {
-        return Math.min.apply(null, array);
     }
+    let findMin = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < findMin) {
+            findMin = array[i];
+        }
+    }
+    return findMin;
 }
 
 
 exports.max = function max(array) {
-    if (array.length == 0 || array == []) {
+    if (!array || array.length === 0) {
         return 0;
-    } else {
-        return Math.max.apply(null, array);
     }
+    let findMax = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (findMax < array[i]) {
+            findMax = array[i];
+        }
+    }
+    return findMax;
 }
 
 exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
     }
-    return sum / array.length;
+    let avg = sum / array.length;
+    return avg;
 }
